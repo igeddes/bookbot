81a26b35ed7fed830f063ce3247691a7988f7a4b
igeddes <iaingeddes@gmail.com>
2024-12-06 23:41:01 +0000

    Addition of count_letters and rename of wordcount

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def wordcount(string):
+def count_words(string: str) -> int:
     # Count the number of words in string
     count = 0
     for word in string.split():
@@ -6,8 +6,22 @@ def wordcount(string):
 
     return count
 
+def count_chars(string: str) -> dict[str, int]:
+    # Count the quantity of each character in a provided string.
+    # Should be case insensitive.
+    count_db = {}
+
+    string = string.lower()
+    for char in string:
+        if char not in count_db:
+            count_db[char] = 0
+        count_db[char] += 1
+
+    return count_db
 
 with open('books/frankenstein.txt') as f:
     file_contents = f.read()
 
-print(wordcount(file_contents))
+print(count_words(file_contents))
+for string in ["aaaa", "aaabbbaaabbb", "This is a test of y'know?"]:
+    print(f"\n{string}\n{count_chars(string)}\n")
