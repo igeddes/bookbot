598eb82a95e8396eb0b4097077b821144a260678
igeddes <iaingeddes@gmail.com>
2024-12-06 02:18:42 +0000

    Fitst pass at wordcouny

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+def wordcount(string):
+    # Count the number of words in string
+    count = 0
+    for word in string.split():
+        count += 1
+
+    return count
+
+
 with open('books/frankenstein.txt') as f:
     file_contents = f.read()
 
-print(file_contents)
+print(wordcount(file_contents))
