3eeb6908e75edb4a7f3970057b00fcb44d0e9b01
igeddes <iaingeddes@gmail.com>
2024-12-07 00:11:09 +0000

    Completed exercise by adding report

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def count_words(string: str) -> int:
 
     return count
 
+
 def count_chars(string: str) -> dict[str, int]:
     # Count the quantity of each character in a provided string.
     # Should be case insensitive.
@@ -19,9 +20,34 @@ def count_chars(string: str) -> dict[str, int]:
 
     return count_db
 
-with open('books/frankenstein.txt') as f:
-    file_contents = f.read()
 
-print(count_words(file_contents))
-for string in ["aaaa", "aaabbbaaabbb", "This is a test of y'know?"]:
-    print(f"\n{string}\n{count_chars(string)}\n")
+def load_book(book_path: str) -> str:
+    with open(book_path) as f:
+        return f.read()
+
+
+def book_report(book_path: str) -> None:
+    try:
+        book_contents = load_book(book_path)
+    except IOError as e:
+        print(f"Report not generated as book could not be loaded: \n {e}")
+    else:
+        print(f"--- Begin report of {book_path} ---\n")
+        print(f"Book contains {count_words(book_contents)} words and the breakdown")
+        print("of letters looks like:\n")
+
+        char_counts = count_chars(book_contents)
+
+        letters = list(char_counts.keys())
+        letters.sort()
+
+        for letter in letters:
+            if letter >= "a" and letter <= "z":
+                print(f"    {letter}:    {char_counts[letter]}")
+
+        print("\n\n--- Report complete ---")
+
+
+book_report("books/frankenstein.txt")
+print("\n\n")
+book_report("books/nobookhere.txt")
